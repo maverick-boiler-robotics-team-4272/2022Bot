062223732745e10d920a84606c323520a8347c7b
Evan Drevalas <eliseecam@gmail.com>
2022-02-09 18:37:37 -0500

    Started intake class

--- src/main/java/frc/robot/Subsystems/Intake.java
+++ src/main/java/frc/robot/Subsystems/Intake.java
@@ -1,5 +1,11 @@
 package frc.robot.Subsystems;
 
+import frc.robot.Robot;
+
 public class Intake {
-    
+    private Robot robot;
+
+    public Intake(Robot robot){
+        this.robot = robot;
+    }
 }
